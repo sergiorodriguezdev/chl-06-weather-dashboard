73298b52090b5187478c860d2e7f6768686e3f45
Sergio <119548442+sergiorodriguezdev@users.noreply.github.com>
2023-03-28 17:15:03 -0600

    Add load current weather data using sample data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-const sampleCurrentDataUrl = "https://api.openweathermap.org/data/2.5/weather?lat=39.66173777978936&lon=-104.74240323970344&appid=2ce42c2263577b2c42cd002a3868e925&units=imperial";
-const sampleForecastDataUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=39.66173777978936&lon=-104.74240323970344&appid=2ce42c2263577b2c42cd002a3868e925&units=imperial";
-
+const currentDataUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apikey}&units=imperial";
+const forecastDataUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={apikey}&units=imperial";
 const apiKey = "2ce42c2263577b2c42cd002a3868e925";
 
 var historyEl = $("#history");
+var currentWeatherEl = $("#weather-today");
 
 function init() {
     loadHistory();
@@ -40,11 +40,51 @@ function loadHistory() {
 }
 
 function loadCurrentData(lat, lon) {
+    var apiUrl = buildApiUrl(currentDataUrl, lat, lon);
 
+    fetch(apiUrl)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+        }).then(function (data) {
+            var cityName = data.name;
+            var currentDate = dayjs.unix(data.dt).format("MM/DD/YYYY");
+            var tempData = data.main.temp;
+            var windData = data.wind.speed;
+            var humidityData = data.main.humidity;
+
+            var weatherHeader = $("<h2>");
+            weatherHeader.text(cityName + " (" + currentDate + ") {icon}");
+
+            var temperatureText = $("<p>");
+            temperatureText.text("Temp: " + tempData + "°F");
+
+            var windText = $("<p>");
+            windText.text("Wind: " + windData + "MPH");
+
+            var humidityText = $("<p>");
+            humidityText.text("Humidity: " + humidityData + "%");
+
+            currentWeatherEl.append(weatherHeader);
+            currentWeatherEl.append(temperatureText);
+            currentWeatherEl.append(windText);
+            currentWeatherEl.append(humidityText);
+            
+        });
 }
 
 function loadForecastData(lat, lon) {
-    
+    var apiUrl = buildApiUrl(forecastDataUrl, lat, lon);
+}
+
+function buildApiUrl(baseUrl, lat, lon) {
+    var apiUrl = baseUrl.replace("{lat}", lat);
+    apiUrl = apiUrl.replace("{lon}", lon);
+    apiUrl = apiUrl.replace("{apikey}", apiKey);
+
+    console.log(apiUrl);
+    return apiUrl;
 }
 
 init();
